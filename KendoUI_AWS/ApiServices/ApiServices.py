8fdc305bbda1ae1928627af3809f773024dd8fe4
joncalfer <jonathan.calderon@ucrso.info>
2016-10-28 14:33:54 -0600

    Creacion de ApiService

---
+++ KendoUI_AWS/ApiServices/ApiServices.py
@@ -0,0 +1,24 @@
+﻿import FincaFilialTable
+
+
+#----------Tabla Usuario----------#
+#Paso 1 - Inicializar script para la base de datos
+fincaFilialTable = FincaFilialTable.FincaFilialTable
+
+#Paso 2 - Crear tabla:
+fincaFilialTable.CreateTable()
+
+#Paso 3 - Precargar usuarios en la tabla:
+fincaFilialTable.InsertData()
+
+#Paso 4 - Iniciar servicios:
+#userServices = UserServices.UserServices
+#userServices.runServices()
+
+#----------Tabla Condominio----------#
+
+#----------Tabla Finca Filial----------#
+
+#ApiServices
+
+
