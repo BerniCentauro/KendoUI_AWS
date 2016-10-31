74c3f15b353b69a37a9d8dfc3392688b08e5570b
Gerardo Aragon <gerardo@centaurosolutions.com>
2016-10-31 16:33:25 -0600

    Controlador y modelo de condominio

---
+++ KendoUI_AWS/MobileInterface/App/Controllers/CondoController.js
@@ -0,0 +1,62 @@
+﻿app.controller('CondoController', ['$scope', 'Condo',
+    function ($scope, Condo) {
+
+        //variables
+
+        var sourceOptions = {
+            transport: {
+                read: function (options) {
+                    $scope.condo.get().then(function (response) {
+                        //success
+                        if (response.status == 200) {
+                            var data = {
+                                items: response.data.Condos.Items,
+                                total: response.data.Condos.Count
+                            }
+
+                            options.success(data);
+                        }
+                    }, function (response) {
+
+                    });
+                }
+            },
+            schema: {
+                data: 'items',
+                total: 'total',
+            }
+        };
+
+        //-----------------------
+        //Scope
+        //-----------------------
+        $scope.condo = new Condo();
+        $scope.source = new kendo.data.DataSource(sourceOptions);
+
+        //------------------------
+        //functions
+        //------------------------
+        $scope.init = function () {
+
+        }
+
+        $scope.add = function () {
+
+            $scope.condo.save().then(function (response) {
+                console.info('Success');
+            }, function (response) {
+                console.error('Error');
+            });
+
+        }
+
+        $scope.edit = function () {
+
+        }
+
+        $scope.delete = function () {
+
+        }
+
+    }
+]);
