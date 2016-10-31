3ec63dc9237792589a582fca12ae29364574588c
Berni Hidalgo <berni@centaurosolutions.com>
2016-10-31 16:26:25 -0600

    Listar y agregar fincas

---
+++ KendoUI_AWS/MobileInterface/App/Controllers/FincaController.js
@@ -0,0 +1,64 @@
+﻿app.controller('FincaController', ['$scope', 'Finca',
+    function ($scope, Finca) {
+
+        // ---------------------------
+        // Variables
+        // ---------------------------
+
+        var sourceOptions = {
+            transport: {
+                read: function (options) {
+                    $scope.finca.get().then(function (response) {
+                        // Success
+                        if(response.status == 200) {
+                            var data = {
+                                items: response.data.FincaFilial.Items,
+                                total: response.data.FincaFilial.Count
+                            }
+
+                            options.success(data);
+                        }
+                    }, function (response) {
+                        // TODO: Handler error
+                    });
+                }
+            },
+            schema: {
+                data: 'items',
+                total: 'total'
+            }
+        };
+
+        // ---------------------------
+        // Scope
+        // ---------------------------
+
+        $scope.finca = new Finca();
+        $scope.source = new kendo.data.DataSource(sourceOptions);
+
+        // ---------------------------
+        // Functions
+        // ---------------------------
+
+        $scope.init = function () {
+
+        }
+
+        $scope.add = function () {
+            $scope.finca.save().then(function (response) {
+                console.info('Success');
+            }, function (response) {
+                console.error('Error');
+            });
+        }
+
+        $scope.edit = function () {
+
+        }
+
+        $scope.delete = function () {
+
+        }
+
+    }
+]);
---
+++ KendoUI_AWS/MobileInterface/App/Models/Finca.js
@@ -0,0 +1,16 @@
+﻿app.factory('Finca', ['DataModel', 'Config',
+    function (DataModel, Config) {
+
+        // Constructor
+        function Finca(data) {
+            if (data) {
+                this.setData(data);
+            }
+        };
+
+        // Methods
+        Finca.prototype = new DataModel(Config.ApiHost + '/api/fincafilial');
+
+        return Finca;
+    }
+]);
